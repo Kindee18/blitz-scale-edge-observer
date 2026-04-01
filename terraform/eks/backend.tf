54076fc5bf20f475bd422c6bd076a975538c3939
Kindson Egbule <kindsonegbule15@gmail.com>
2026-04-01 14:35:14 +0100

    terraform: switch EKS backend to accessible state bucket

--- terraform/eks/backend.tf
+++ terraform/eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "blitz-terraform-state-prod"
+    bucket         = "blitz-terraform-state-599626781403"
     key            = "eks/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "blitz-terraform-locks"
