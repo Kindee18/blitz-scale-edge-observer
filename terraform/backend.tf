dd3eb2fc8e85489a3312c13ef76a910616124d70
Kindson Egbule <kindsonegbule15@gmail.com>
2026-04-02 02:47:38 +0100

    fix(terraform): use correct state bucket region for shared stacks

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "blitz-terraform-state-599626781403"
     key     = "terraform/root/terraform.tfstate"
-    region  = "us-east-1"
+    region  = "us-west-2"
     encrypt = true
   }
 }
